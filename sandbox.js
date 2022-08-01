45c6b371f904a03016ca3baed4894aa86b846c75
sammie <51814698+ogoigbe12@users.noreply.github.com>
2022-08-01 12:48:29 +0100

    new commit

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+var character = 'sammie';
+var age = 30;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(23));
+// Arrays
+var names = ['sammie', 'abigirl', 'lama'];
+names.push('earza');
+console.log(names);
+var mixed = ['sam', 5, 'chun-li', 6, true];
+mixed.push('goku');
+mixed.push(23);
+mixed[2] = 'Mc';
+mixed[1] = 2;
+console.log(mixed);
+//obejects
+var ninja = {
+    name: 'sammie',
+    age: '25',
+    belt: 'white'
+};
+ninja.name = "";
+ninja.age = "";
+ninja.belt = "";
---
+++ sandbox.ts
@@ -0,0 +1,37 @@
+let character = 'sammie';
+let age = 30;
+
+
+
+const circ = (diameter: number)=>{
+    return diameter * Math.PI
+}
+console.log(circ(23))
+
+// Arrays
+
+let names = ['sammie', 'abigirl', 'lama']
+
+names.push('earza')
+
+console.log(names)
+
+let mixed = ['sam', 5, 'chun-li', 6, true]
+mixed.push('goku')
+mixed.push(23)
+mixed[2]='Mc'
+mixed[1]=2
+console.log(mixed);
+
+//obejects
+let ninja = {
+    name: 'sammie',
+    age: '25',
+    belt: 'white'
+}
+
+ninja.name = ""
+ninja.age = ""
+ninja.belt = ""
+
+
