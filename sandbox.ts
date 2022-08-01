b6ff21880f13397856a31f5757c74c20257833b4
sammie <51814698+ogoigbe12@users.noreply.github.com>
2022-08-01 13:40:47 +0100

    lesson-3

--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,42 @@
-let character = 'sammie';
-let age = 30;
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean
 
+age = 30
 
+isLoggedIn = true
 
-const circ = (diameter: number)=>{
-    return diameter * Math.PI
-}
-console.log(circ(23))
+character = 'sammie'
 
-// Arrays
+//arrays
+ let  sammie: string[] = []
+ let oldage: number[] = []
+sammie.push('yosh')
+oldage.push[23]
 
-let names = ['sammie', 'abigirl', 'lama']
+//  sammie = ['sammie']
 
-names.push('earza')
-
-console.log(names)
-
-let mixed = ['sam', 5, 'chun-li', 6, true]
-mixed.push('goku')
+//union types for array we need ()
+let mixed: (string|number|boolean)[]=[]
+mixed.push('manu')
 mixed.push(23)
-mixed[2]='Mc'
-mixed[1]=2
+mixed.push(true)
 console.log(mixed);
 
-//obejects
-let ninja = {
-    name: 'sammie',
-    age: '25',
-    belt: 'white'
+let mid: string|boolean
+mid='sammie'
+mid= true
+
+
+
+//objects
+let sammieOne: object
+sammieOne = {name:'abigirl', age: 18}
+
+let sammieTwo: {
+    name: string,
+    age: number,
+    beltColor: string
 }
-
-ninja.name = ""
-ninja.age = ""
-ninja.belt = ""
-
-
+sammieTwo={name: 'lama',age: 20, beltColor: 'green'}
