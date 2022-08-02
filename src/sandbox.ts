06576aa3d1a7531dfa3a09a85291366734ad812e
sammie <51814698+ogoigbe12@users.noreply.github.com>
2022-08-02 12:31:02 +0100

    lesson-7

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,13 +1,29 @@
-// type aliases
-type StringorNum = string|Number
-type objwithNum = {name:string, uid: string|number}
+// function signatures
 
+//example 1
+let greet: (a: string, b: string) => void
 
-const longDetails = (uid: StringorNum, item: string)=>{
-    console.log(`${item} has a uid of ${uid}`)
+greet= (name: string, greeting: string)=>{
+    console.log(`${name} say hello, ${greeting}`)
 }
 
-const greet = (user: objwithNum)=>{
-    console.log(`${user. name} says hi`);
+//example 2
+let calc: (a: number, b: number, c: string) => number
+
+calc= (numOne: number, numTwo: number, action: string)=>{
+    if(action === 'add'){
+        return numOne + numTwo
+    }else{
+        return numOne - numTwo
+    }
     
+}
+
+//example3
+let logDetials: (obj: {name: string, age: number})=> void
+
+type person = {name: string, age: number}
+
+logDetials= (sammie:person) => {
+    console.log(`${sammie.name} is ${sammie.age} years old`)
 }
