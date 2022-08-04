44ff78a621f4345c302d04e6678bfdfc05c79175
sammie <51814698+ogoigbe12@users.noreply.github.com>
2022-08-04 14:23:45 +0100

    update test.ts with interfaces.ts

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,25 @@
-console.log('test1,2,3,5');
+//interfaces test code
+interface IsPerson {
+    name: string,
+    age: number,
+    speak(a: string): void
+    spend(b: number): number
+}
+
+const me: IsPerson ={
+    name: 'sammie',
+    age: 27,
+    speak(text: string): void{
+        console.log(text)
+    },
+    spend(amount: number): number{
+        console.log('i spent', amount)
+        return amount
+    }
+}
+
+const greetPerson = (person: IsPerson) => {
+    console.log('hello', person.name)
+}
+greetPerson(me)
+console.log(me);
