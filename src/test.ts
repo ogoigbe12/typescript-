f98353cff69ff3902763125854f09599cf139eff
sammie <51814698+ogoigbe12@users.noreply.github.com>
2022-08-04 15:56:19 +0100

    GENERICS examples

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,31 @@
-console.log('test1,2,3,5');
+//GENERICS
+
+const addUID = <T extends {name: string}>(obj: T)=>{
+    let uid = Math.floor(Math.random()*100)
+    return {...obj, uid}
+}
+
+let docOne = addUID({name:'sammie', age: 26})
+
+console.log(docOne.age);
+
+//GENERICS WITH interfaces
+interface Resource<T>{
+    uid: number
+    resorceName: string
+    data: T
+}
+
+const docThree: Resource<object> ={
+    uid: 1,
+    resorceName: 'person',
+    data: {name: 'sammie'}
+
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resorceName: 'soccer list',
+    data: ['boot', 'short', 'top' ]
+}
+console.log(docThree, docFour);
